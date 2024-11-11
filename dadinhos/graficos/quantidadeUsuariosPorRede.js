b4f6bb5ccadd113125877231516b1cb6b43f4dad
AndyZahk <107253856+AndyZahk@users.noreply.github.com>
2024-11-11 11:08:52 -0300

    Add files via upload

--- dadinhos/graficos/quantidadeUsuariosPorRede.js
+++ dadinhos/graficos/quantidadeUsuariosPorRede.js
@@ -5,12 +5,16 @@ async function quantidadeUsuariosPorRede() {
     const res = await fetch (url)
     const dados = await res.json()
     const nomeDasRedes = Object.keys(dados);
+    const quantidadeDeUsuarios = Object.values(dados);
     
     const data = [
         {
             x: nomeDasRedes,
             y: quantidadeDeUsuarios,
-            type: 'bar'
+            type: 'bar',
+            marker: {
+                color: "purple"
+            }
         }
     ]
 
