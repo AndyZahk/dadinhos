b5c2667f6a8cd2b64984ba8686d3d0a0913526c5
AndyZahk <107253856+AndyZahk@users.noreply.github.com>
2024-12-04 09:09:41 -0300

    Add files via upload

---
+++ dadinhos/graficos/users_Value.js
@@ -0,0 +1,51 @@
+import { getCSS, tickConfig } from './common.js';
+
+
+async function users_network() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json';
+    const res = await fetch(url);
+    const dados = await res.json();
+    const nome_Redes = Object.keys(dados);
+    const quantidade_Users = Object.values(dados);
+
+    const data = [
+        {
+            x: nome_Redes,
+            y: quantidade_Users,
+            type: 'bar',
+            marker: {
+                color: "purple",
+            }
+        }
+    ];
+
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color'),
+        xaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'nome das redes sociais',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'bilhões de usuários ativos',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        }
+    };
+
+    const grafico = document.createElement('div');
+    grafico.className = 'graficos';
+    document.getElementById('graficos-container').appendChild(grafico);
+    Plotly.newPlot(grafico, data, layout)
+}
+
+users_network()
